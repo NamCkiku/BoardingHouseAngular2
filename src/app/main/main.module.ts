f8227ed8a4721bd661f6d9f0dbf1d817b49c9076
Trần Hoàng Nam <namth@hn-binhanh.local>
2017-05-30 17:17:25 +0700

    ghep giao dien chinh

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,9 +4,11 @@ import { MainComponent } from './main.component';
 import { RouterModule, Routes } from '@angular/router';
 import { mainRoutes } from './main.routes';
 import { UserModule } from './user/user.module';
+import { HomeModule } from './home/home.module';
 @NgModule({
   imports: [
     CommonModule,
+    HomeModule,
     UserModule,
     RouterModule.forChild(mainRoutes)
   ],
